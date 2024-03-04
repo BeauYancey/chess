29f65faa897bc6e5373bc0beba42660cb13d161e
Beau Yancey <yancey.beau.j@gmail.com>
2024-03-04 11:21:43 -0700

    [4] implemented SQLUserDAO.removeAll and SQLUserDAO.addUser

--- server/src/main/java/dataAccess/SQLUserDAO.java
+++ server/src/main/java/dataAccess/SQLUserDAO.java
@@ -10,9 +10,10 @@ public class SQLUserDAO implements UserDAO {
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
-        String query = String.format("SELECT username, password, email FROM users WHERE username='%s'", username);
+        String query = "SELECT username, password, email FROM users WHERE username=?";
         try (var conn = DatabaseManager.getConnection()) {
             try (var preparedStatement = conn.prepareStatement(query)) {
+                preparedStatement.setString(1, username);
                 var rs = preparedStatement.executeQuery();
                 if (rs.next()) {
                     String password = rs.getString(2);
@@ -29,12 +30,34 @@ public class SQLUserDAO implements UserDAO {
     }
 
     @Override
-    public void addUser(UserData userData) {
+    public void addUser(UserData userData) throws DataAccessException{
+        String statement = "INSERT INTO users (username, password, email) values (?, ?, ?)";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.setString(1, userData.username());
+                preparedStatement.setString(2, userData.password());
+                preparedStatement.setString(3, userData.email());
 
+                int rowsAffected = preparedStatement.executeUpdate();
+                if (rowsAffected != 1) {
+                    throw new DataAccessException("SQL: Adding new user affected " + rowsAffected + " rows");
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
     }
 
     @Override
-    public void removeAll() {
-
+    public void removeAll() throws DataAccessException {
+        String statement = "DELETE FROM users";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.executeUpdate();
+            }
+        }
+        catch (SQLException ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
     }
 }
--- server/src/test/java/dataAccess/DataAccessTests.java
+++ server/src/test/java/dataAccess/DataAccessTests.java
@@ -2,23 +2,56 @@ package dataAccess;
 
 import model.UserData;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class DataAccessTests {
 
-    SQLUserDAO userDAO = new SQLUserDAO();
+    UserDAO userDAO = new SQLUserDAO();
+
+    @BeforeEach
+    public void reset() throws DataAccessException{
+        userDAO.removeAll();
+    }
 
     @Test
-    public void getTestUser() {
-        UserData user = null;
+    public void getUserTest() {
         try {
-            user = userDAO.getUser("test-user");
+            UserData user = userDAO.getUser("test-user");
+            Assertions.assertNotNull(user);
+            Assertions.assertEquals(user.password(), "test-pass");
+            Assertions.assertEquals(user.email(), "test-email");
         }
         catch (DataAccessException ex) {
             Assertions.fail();
         }
-        Assertions.assertNotNull(user);
-        Assertions.assertEquals(user.password(), "test-pass");
-        Assertions.assertEquals(user.email(), "test-email");
+    }
+
+    @Test
+    public void addUserTest() {
+        try {
+            UserData newUser = new UserData("test-user-2", "test-pass-2", "test-email-2");
+            userDAO.addUser(newUser);
+
+            UserData result = userDAO.getUser("test-user-2");
+            Assertions.assertNotNull(result);
+            Assertions.assertEquals(result.password(), "test-pass-2");
+            Assertions.assertEquals(result.email(), "test-email-2");
+        }
+        catch (DataAccessException ex) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    public void removeAllTest() {
+        try {
+            userDAO.removeAll();
+            UserData testUser = userDAO.getUser("test-user");
+            Assertions.assertNull(testUser);
+        }
+        catch (DataAccessException ex) {
+            Assertions.fail();
+        }
     }
 }
